d2f6bd9b7411fa066b8594d354c0e718cae2f320
hamid <hamidgokce@gmail.com>
2021-06-22 11:09:18 +0300

    convert time

---
+++ python/coding-challenges/cc-002-convert-millisecs-to-hours-mins-secs/cc-002-convert-millisecs-to-hours-mins-secs.py
@@ -0,0 +1,10 @@
+millisecond = int(input('Please write a number as a millisecond d :'))
+
+def converter:
+    
+    
+     
+    while True:
+        if millisecond < 0
+            return ('Not Valid Input!!!')
+        
