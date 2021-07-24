afc6a375d604518838d2ccfa134637c77ff019f6
hamid <hamidgokce@gmail.com>
2021-07-25 00:27:04 +0300

    redirect/url for in Flask

---
+++ individual_study/redirect- URL for/redirect.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/toplam", methods = ['GET', 'POST']) # bir sayfada get ve post request olacaksa bunu mutlaka belirtmek gerekmektedir.
+def toplam(): # requestimizin get mi post mu oldugunu anlamaya calisacagiz
+    if request.method == 'POST':  
+        number1 = request.form.get('number1') # bize gelecek olan degiskendeki degerleri request.form ile alabiliriz.
+        number2 = request.form.get('number2')
+        return render_template("number.html", total = int(number1) + int(number2))
+    else:
+        return redirect(url_for('index')) # index fonksiyonunun iliskili oldugu url ye git anlaminda
+
+if __name__ == '__main__':
+    app.run(debug=True)
