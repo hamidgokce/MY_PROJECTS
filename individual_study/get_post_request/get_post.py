8489d3c990541e37b361f73b2a08edac53c15a54
hamid <hamidgokce@gmail.com>
2021-07-24 19:39:21 +0300

    get/post py in flask

--- individual_study/get_post_request/get_post.py
+++ individual_study/get_post_request/get_post.py
@@ -5,10 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/toplam", methods = ['GET', 'POST'])
-def toplam():
-    if request.method == 'POST': 
-        number1 = request.form.get('number1')
+@app.route("/toplam", methods = ['GET', 'POST']) # bir sayfada get ve post request olacaksa bunu mutlaka belirtmek gerekmektedir.
+def toplam(): # requestimizin get mi post mu oldugunu anlamaya calisacagiz
+    if request.method == 'POST':  
+        number1 = request.form.get('number1') # bize gelecek olan degiskendeki degerleri request.form ile alabiliriz.
         number2 = request.form.get('number2')
         return render_template("number.html", total = int(number1) + int(number2))
     else:
