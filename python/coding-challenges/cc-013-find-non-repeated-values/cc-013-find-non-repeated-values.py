0c252543896b8dcff84492613b027553a940a60a
hamid <hamidgokce@gmail.com>
2021-08-13 19:53:25 +0300

    Find the Non-Repeated Values

--- python/coding-challenges/cc-013-find-non-repeated-values/cc-013-find-non-repeated-values.py
+++ python/coding-challenges/cc-013-find-non-repeated-values/cc-013-find-non-repeated-values.py
@@ -11,3 +11,24 @@ def get_unique_values(products):
             result.append(i)
     return result
 print(get_unique_values(products))
+
+
+products = ["Pride and Prejudice", "To Kill a Mockingbird", "The Great Gatsby", "One Hundred Years of Solitude", \
+            "Pride and Prejudice", "In Cold Blood", "Wide Sargasso Sea", \
+            "One Hundred Years of Solitude", "Brave New World",  "The Great Gatsby", "Brave New World", \
+            "I Capture The Castle", "Brave New World", "The Great Gatsby", "The Great Gatsby", \
+            "One Hundred Years of Solitude", "Pride and Prejudice"]
+for product_1 in products:
+  count = 0
+  for product_2 in products:
+    if product_1 == product_2:
+      count += 1
+      if count > 1:
+        break
+  if count == 1:
+    print(product_1)
+
+
+
+
+
