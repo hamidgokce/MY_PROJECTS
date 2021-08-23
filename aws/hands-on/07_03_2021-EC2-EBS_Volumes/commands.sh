52ff83e3845cb427eab67d59cfe75814580e87ec
hamid <hamidgokce@gmail.com>
2021-08-23 14:34:18 +0300

    elb_command.sh

--- aws/hands-on/07_03_2021-EC2-EBS_Volumes/commands.sh
+++ aws/hands-on/07_03_2021-EC2-EBS_Volumes/commands.sh
@@ -21,5 +21,40 @@ Df -h
 sudo mount /dev/xvdf /mnt/2nd-vol/
 Lsblk
 Df -h
+## AWS console volume modify
+Sudo fdisk -l # list of storages
+sudo fdisk /dev/xvdg # partition process
+Lsblk
+sudo mkfs -t ext4 /dev/xvdg1
+sudo mkfs -t ext4 /dev/xvdg2
+sudo mkdir /mnt/3rd-vol-part1
+sudo mkdir /mnt/3rd-vol-part2
+sudo mount /dev/xvdg1 /mnt/3rd-vol-part1
+sudo mount /dev/xvdg2 /mnt/3rd-vol-part2
+Lsblk
+cd /mnt/3rd-vol-part2
+sudo touch gokcewashere.txt
+Ls
+## AWS console volume modify 1 ==> 2
+sudo growpart /dev/xvdg 2
+sudo resize2fs /dev/xvdg2
+cd /mnt/3rd-vol-part2
+Ls
+Sudo reboot now
+Lsblk
+Cd /etc
+Ls
+Cat fstab
+Sudo cp fstab fstab.bak
+Sudo Nano fstab # for permanent changes
+<
+/dev/xvdf       mnt/mp1   ext4    defaults,nofail        0       0
+ /dev/xvdg1      mnt/mp2   ext4  defaults,nofail        0       0
+ /dev/xvdg2      mnt/mp3   ext4  defaults,nofail        0       0
+>
+sudo reboot now
+Lsblk
+
+
 
 
