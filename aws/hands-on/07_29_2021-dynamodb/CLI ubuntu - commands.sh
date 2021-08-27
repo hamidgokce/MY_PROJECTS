2c858be6f0d6d846af3d91c738578b26648973ec
hamid <hamidgokce@gmail.com>
2021-08-27 22:26:22 +0300

    cli ubuntu commands

---
+++ aws/hands-on/07_29_2021-dynamodb/CLI ubuntu - commands.sh
@@ -0,0 +1,13 @@
+# CLI ubuntu - commands
+
+sudo apt install unzip
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+
+unzip awscliv2.zip
+
+sudo ./aws/install
+
+aws configure
+
+aws ec2 describe-instances
